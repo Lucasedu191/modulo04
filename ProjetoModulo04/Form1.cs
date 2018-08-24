640f4e94e0841c07a3b4ef99824db708a5bc1739
Lucasedu191 <lucasatletico@hotmail.com>
2018-08-24 17:38:34 -0300

    tipos de dados

--- ProjetoModulo04/Form1.cs
+++ ProjetoModulo04/Form1.cs
@@ -12,9 +12,32 @@ namespace ProjetoModulo04
 {
     public partial class Form1 : Form
     {
+        //variaveis de tipo global, definir se é privado e pode acessar de fora a variavel
+        // tipo public ou private
+        public const double pi = 3.1415;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int numero1 = 12;
+            int numero2 = 20;
+            int soma = numero1 + numero2;
+            int soma2 = 12 + 20;
+            //é colocado o f no fim do numero para evidenciar que é flutuante
+            float var1 = 1.5f;
+            double var2 = 1.5;
+            //é colocado constante antes do tipo da variavel para evidenciar 
+            //que não pode alterar o valor antes da execução
+            const double pi = 3.1415;
+            //tipo caracter colocar tipo aspas simples
+            char var3 = 'b';
+            //tipo palavras colocar aspas duplas
+            string var4 = "teste";
         }
     }
 }

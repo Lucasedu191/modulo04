aeaff74810b4731c49be8d7f4217ca3fc61efba9
Lucasedu191 <lucasatletico@hotmail.com>
2018-08-25 00:10:20 -0300

    utilizando outras formas de loop, while, do while, for e foreach

--- ProjetoModulo04/frmEstruturasRepeticao.Designer.cs
+++ ProjetoModulo04/frmEstruturasRepeticao.Designer.cs
@@ -31,6 +31,12 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.txtTabuada = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -47,14 +53,14 @@
             // txtTabuada
             // 
             this.txtTabuada.BackColor = System.Drawing.Color.Lime;
-            this.txtTabuada.Location = new System.Drawing.Point(364, 60);
+            this.txtTabuada.Location = new System.Drawing.Point(364, 6);
             this.txtTabuada.Name = "txtTabuada";
             this.txtTabuada.Size = new System.Drawing.Size(100, 20);
             this.txtTabuada.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(376, 100);
+            this.button1.Location = new System.Drawing.Point(376, 46);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -62,11 +68,74 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Lime;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(364, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Aqua;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(364, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(114, 82);
+            this.listBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Aqua;
+            this.textBox2.Location = new System.Drawing.Point(364, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aqua;
+            this.button2.Location = new System.Drawing.Point(364, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.Aqua;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(364, 270);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(114, 82);
+            this.listBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Aqua;
+            this.button3.Location = new System.Drawing.Point(364, 249);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // frmEstruturasRepeticao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 345);
+            this.ClientSize = new System.Drawing.Size(478, 355);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtTabuada);
             this.Controls.Add(this.listView1);
@@ -83,5 +152,11 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.TextBox txtTabuada;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- ProjetoModulo04/frmEstruturasRepeticao.cs
+++ ProjetoModulo04/frmEstruturasRepeticao.cs
@@ -12,11 +12,13 @@ namespace ProjetoModulo04
 {
     public partial class frmEstruturasRepeticao : Form
     {
+        private List<int> lista = new List<int>();
+
         public frmEstruturasRepeticao()
         {
             InitializeComponent();
         }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
             //mensagem de erro quando esta vazio o txt box
@@ -30,20 +32,52 @@ namespace ProjetoModulo04
             int iValorLido = Convert.ToInt32(txtTabuada.Text.Trim());
             //limpa o listview 
             listView1.Items.Clear();
+            //criar variavel para while
+            int i = 0;
             //utiliza a estrutura de repetição  for para 11 posições e faz a separação do sinal de vezes e de igual pelas colunas
-            for(int i =0; i <=10; i++)
+            /* 
+             for(int i =0; i <=10; i++)
+             {
+                 ListViewItem itmx = listView1.Items.Add(iValorLido.ToString());
+                 itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, "*"));
+                 itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, i.ToString()));
+                 itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, "="));
+                 itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, (iValorLido * i).ToString()));
+             }
+             */
+
+            // bloco while enquanto ele for dentro do numero que o limita o while ele vai continuar o loop
+
+            /*
+            while (i <= 10)
             {
                 ListViewItem itmx = listView1.Items.Add(iValorLido.ToString());
                 itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, "*"));
                 itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, i.ToString()));
                 itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, "="));
                 itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, (iValorLido * i).ToString()));
+                // ele soma a variavel e volta ao inicio do bloco
+                i++;
             }
+            */
 
+            //ele vai fazer o codigo pelo menos uma vez depois ira fazer a verificação
+            do
+            {
+                ListViewItem itmx = listView1.Items.Add(iValorLido.ToString());
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, "*"));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, i.ToString()));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, "="));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, (iValorLido * i).ToString()));
+                // ele soma a variavel e volta ao inicio do bloco
+                i++;
+            } while (i <= 10);
         }
 
         private void frmEstruturasRepeticao_Load(object sender, EventArgs e)
         {
+            //variavel para o while
+            int i = 0;
             //limpa as informaçoes do listview
             listView1.Clear();
             //valor1 sinal valor2 = resultado
@@ -53,6 +87,51 @@ namespace ProjetoModulo04
             listView1.Columns.Insert(2, " Valor", 40, HorizontalAlignment.Center);
             listView1.Columns.Insert(3, " igual", 40, HorizontalAlignment.Center);
             listView1.Columns.Insert(4, " resultado", 40, HorizontalAlignment.Center);
+            /*
+            for (int i = 0; i <=100; i++)
+            {
+                comboBox1.Items.Add(i.ToString());
+            }
+            */
+
+            /*
+            while(i <= 100)
+            {
+                comboBox1.Items.Add(i.ToString());
+                i++;
+            }
+            */
+
+            do
+            {
+                comboBox1.Items.Add(i.ToString());
+                i++;
+            } while (i <= 100);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Trim().Equals(string.Empty))
+            {
+                MessageBox.Show("Você devera informar um valor!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Focus();
+                return;
+            }
+            lista.Add(Convert.ToInt32(textBox2.Text.Trim()));
+            listBox1.Items.Add(textBox2.Text.Trim());
+            textBox2.Text = string.Empty;
+            textBox2.Focus();
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //foreach faz a execução do mesmo codigo diversas vezes sm que precisa mudar o 
+            // codigo
+            foreach (string item in listBox1.Items)
+            {
+                listBox2.Items.Add(item);
+            }
         }
     }
 }

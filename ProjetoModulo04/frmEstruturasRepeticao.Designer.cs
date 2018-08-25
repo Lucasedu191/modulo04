c99a0b38f8343760f883f7ac7c443b1e946b77fa
Lucasedu191 <lucasatletico@hotmail.com>
2018-08-24 23:29:28 -0300

    Criando a estrutura de repetição for e criando o formulario de uma tabuada

---
+++ ProjetoModulo04/frmEstruturasRepeticao.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ProjetoModulo04
+{
+    partial class frmEstruturasRepeticao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtTabuada = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(24, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(334, 301);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // txtTabuada
+            // 
+            this.txtTabuada.BackColor = System.Drawing.Color.Lime;
+            this.txtTabuada.Location = new System.Drawing.Point(364, 60);
+            this.txtTabuada.Name = "txtTabuada";
+            this.txtTabuada.Size = new System.Drawing.Size(100, 20);
+            this.txtTabuada.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmEstruturasRepeticao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 345);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTabuada);
+            this.Controls.Add(this.listView1);
+            this.Name = "frmEstruturasRepeticao";
+            this.Text = "frmEstruturasRepeticao";
+            this.Load += new System.EventHandler(this.frmEstruturasRepeticao_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtTabuada;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ProjetoModulo04/frmEstruturasRepeticao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoModulo04
+{
+    public partial class frmEstruturasRepeticao : Form
+    {
+        public frmEstruturasRepeticao()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //mensagem de erro quando esta vazio o txt box
+            if (txtTabuada.Text.Trim().Equals(string.Empty))
+            {
+                MessageBox.Show("Você devera informar um valor!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtTabuada.Focus();
+                return;
+            }
+            //pega o valor do textbox 
+            int iValorLido = Convert.ToInt32(txtTabuada.Text.Trim());
+            //limpa o listview 
+            listView1.Items.Clear();
+            //utiliza a estrutura de repetição  for para 11 posições e faz a separação do sinal de vezes e de igual pelas colunas
+            for(int i =0; i <=10; i++)
+            {
+                ListViewItem itmx = listView1.Items.Add(iValorLido.ToString());
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, "*"));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, i.ToString()));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, "="));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, (iValorLido * i).ToString()));
+            }
+
+        }
+
+        private void frmEstruturasRepeticao_Load(object sender, EventArgs e)
+        {
+            //limpa as informaçoes do listview
+            listView1.Clear();
+            //valor1 sinal valor2 = resultado
+            //criação de colunas com tamanho 
+            listView1.Columns.Insert(0, " Valor", 40, HorizontalAlignment.Center);
+            listView1.Columns.Insert(1, " sinal", 40, HorizontalAlignment.Center);
+            listView1.Columns.Insert(2, " Valor", 40, HorizontalAlignment.Center);
+            listView1.Columns.Insert(3, " igual", 40, HorizontalAlignment.Center);
+            listView1.Columns.Insert(4, " resultado", 40, HorizontalAlignment.Center);
+        }
+    }
+}
